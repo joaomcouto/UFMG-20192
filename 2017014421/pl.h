fbacace03380ebff80825997638124ef8c6ca5e0
Joao Couto <joaom.couto55@gmail.com>
2019-09-27 18:05:11 -0300

    Directory migration and delivery version

---
+++ 2017014421/pl.h
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+class PL{
+    private:
+    public:
+        int _numVariables ;
+        int _numRestrictions ;
+        int _numRows ;
+        int _numColumns ;
+        double ** _matrix ;
+        double * vero ; 
+        PL(int numRestrictions, int numVariables,FILE * entrada, bool auxiliar);
+        ~PL();
+        void printMatrix();
+        int bNegative();
+        int findPivotColumn();
+        int findPivotRow(int pivotColumn);
+        void pivoting(int row, int col);
+        void printVero();
+        void printSolutionValue();
+        void printSolution();
+        void printBoundlessCertificate(int pivotColumn);
+        double fetchSolutionValue();
+        void copyAuxiliar(PL * auxiliar) ;
+
+
+    };
+    
+    
